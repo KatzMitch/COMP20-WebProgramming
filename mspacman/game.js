4feaf41b7852e2a7798b64c66e9061dc882fe3ea
Ming Chow <mchow@cs.tufts.edu>
2015-02-25 21:20:01 -0500

    Added event to draw only when image is fully loaded

--- mspacman/game.js
+++ mspacman/game.js
@@ -2,6 +2,12 @@ function init() {
         var ctx = document.getElementById('game_canvas').getContext('2d');
         var img = new Image();
         img.src = "pacman10-hp-sprite.png";
-        ctx.drawImage(img, 325, 0, 465, 137, 0, 0, 465, 137);
-        ctx.drawImage(img, 80, 20, 17, 20, 30, 30, 17, 20);
+
+        // Refer to my note under "Notes and Hints" on Lab 5
+        // More: https://stackoverflow.com/questions/10121797/javascript-image-onload-event-binding
+        // Another way, same idea: https://stackoverflow.com/questions/280049/javascript-callback-for-knowing-when-an-image-is-loaded
+        img.addEventListener("load", function() {
+        	ctx.drawImage(img, 325, 0, 465, 137, 0, 0, 465, 137);
+    	    ctx.drawImage(img, 80, 20, 17, 20, 30, 30, 17, 20);
+		}, false);
 }
