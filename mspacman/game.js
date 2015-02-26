fc0638838a72fb71a3a65894c7f45f6bbe800707
KatzMitch <mitchellmkatz@gmail.com>
2015-02-25 20:59:21 -0500

    Have up what I think should work...

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,7 @@
-// Replace this with your code...
+function init() {
+        var ctx = document.getElementById('game_canvas').getContext('2d');
+        var img = new Image();
+        img.src = "pacman10-hp-sprite.png";
+        ctx.drawImage(img, 325, 0, 465, 137, 0, 0, 465, 137);
+        ctx.drawImage(img, 80, 20, 17, 20, 30, 30, 17, 20);
+}
