2c121beab8868f5f398fe39f3e58abe0bd39b153
KatzMitch <mitchellmkatz@gmail.com>
2015-03-07 16:43:35 -0500

    Only parse on ready stage 4

--- messages/lab.js
+++ messages/lab.js
@@ -2,10 +2,12 @@ function parse() {
         var request = new XMLHttpRequest();
         request.open("GET", "data.json", true);
         request.onreadystatechange = function() {
-                json = request.responseText;
-                parsed = JSON.parse(json);
-                for (x in parsed) {
-                        console.log(parsed[x].content + " " + parsed[x].username);
+                if (request.readyState == 4) {
+                        json = request.responseText;
+                        parsed = JSON.parse(json);
+                        for (x in parsed) {
+                                console.log(parsed[x].content + " " + parsed[x].username);
+                        }
                 }
         }
         request.send();
