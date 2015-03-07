d3cfd7057aedc1e4ea02cb3f5cb36568c3e815f7
KatzMitch <mitchellmkatz@gmail.com>
2015-03-07 16:26:36 -0500

    Wrote lab.js

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,13 @@
-// Your JavaScript goes here...
+function parse() {
+        var request = new XMLHttpRequest();
+        request.open("GET", "data.json", true);
+        request.onreadystatechange = function() {
+                if (request.)
+                json = request.responseText;
+                parsed = JSON.parse(json);
+                for (x in parsed) {
+                        console.log(x["id"]["content"]);
+                }
+        }
+        request.send();
+}
