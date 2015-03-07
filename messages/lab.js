b64c594e5141dd126e99fe2f5fc348e3422c2c06
KatzMitch <mitchellmkatz@gmail.com>
2015-03-07 17:52:11 -0500

    Added missing semicolon

--- messages/lab.js
+++ messages/lab.js
@@ -7,7 +7,7 @@ function parse() {
                         parsed = JSON.parse(json);
                         for (x in parsed) {
                                 html = document.getElementByID("messages")
-                                html.innerHTML += "<h2>" + parsed[x].content + "</h2><p>" + parsed[x].username + "</p>"
+                                html.innerHTML += "<h2>" + parsed[x].content + "</h2><p>" + parsed[x].username + "</p>";
                                 //console.log(parsed[x].content + " " + parsed[x].username);
                         }
                 }
