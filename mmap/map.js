8ccc0316d1f372ffaa8fdac5f4167de73e44675f
KatzMitch <mitchellmkatz@gmail.com>
2015-03-10 22:12:49 -0400

    Wayward semicolon fix

--- mmap/map.js
+++ mmap/map.js
@@ -58,6 +58,9 @@ function drawMap() {
                 });
                 marker.setMap(map);
                 google.maps.event.addListener(marker, "click", function () {
+                        infowindow = new google.maps.InfoWindow({
+                                content: marker.getTitle()
+                        });
                         infowindow.setContent(marker.title);
                         infowindow.open(map, marker);
                         map.panTo(marker);
